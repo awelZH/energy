149cca41f8b4a44206bb8f81bba4bdfba220b6a9
Lara Schreier <lara.schreier@zh.ch>
2025-02-17 12:35:08 +0100

    added canton

--- analyse/03_data_gest.R
+++ analyse/03_data_gest.R
@@ -1,36 +1,21 @@
----
-title: "Automatisierung der Gesamtenergie- & Verkehrsstatistik"
-author: "Lara Schreier"
-format: html
-editor: visual
----
+### Automate Energiestatistik: csv
 
-## Automatisierung der Energiestatistik: Gesamt & Verkehr
-
-## 1. Inputdaten
-
-```{r}
-#| echo: false
-
-# save code as 03_data_gest here: C:\Users\Public\Git Repos\energy\analyse
+# save code here: C:\Users\Public\Git Repos\energy\analyse
+# save code as 03_data_gest
 
 # clear workspace without unloading packages
 rm(list = ls())
 
-# install tidyverse - we need dplyr and readr
-# install.packages("tidyverse") 
+# install packages: we need dplyr and readr
+install.packages("tidyverse")
 
+# load libraries
 library(readr)
 library(dplyr)
-```
 
-Es werden Daten von https://opendata.swiss/de/dataset/energiebilanz-der-schweiz verwendet:
-
-```{r}
+# read data from https://opendata.swiss/de/dataset/energiebilanz-der-schweiz
 data <- read_csv('https://www.uvek-gis.admin.ch/BFE/ogd/115/ogd115_gest_bilanz.csv')
-```
 
-```{r}
 # check names
 names(data)
 
@@ -38,11 +23,7 @@ names(data)
 unique(data$Jahr)
 unique(data$Rubrik)
 unique(data$Energietraeger)
-```
 
-## 2. Datenverarbeitung
-
-```{r}
 # create subset for: 1990+, Erdölprodukte, Total
 subset1 <- data %>%
   filter(Jahr >= 1990 & Rubrik == "Endverbrauch - Total" & Energietraeger == "Erdölprodukte")
@@ -74,10 +55,8 @@ subset6 <- data %>%
 # create subset for: 1990+, Verkehr, Übrige Erneuerbare
 subset7 <- data %>%
   filter(Jahr >= 1990 & Rubrik == "Endverbrauch - Verkehr" & Energietraeger == "Uebrige erneuerbare Energien")
-```
 
-```{r}
-# create Rubrik "Heizöl" = DIFFERENCE BETWEEN "Endverbrauch - Total" (Rubrik) // "Erdölprodukte" (Energieträger) AND "Endverbrauch - Verkehr" (Rubrik) // "Erdölprodukte" (Energieträger)
+#########
 
 # filter data for Rubrik = "Endverbrauch - Verkehr" and Energietraeger = "Erdölprodukte"
 data_x_y <- data %>%
@@ -103,10 +82,7 @@ combined_data <- fuel_data %>%
   select(Jahr, TJ_x_y, TJ_a_y, difference, Rubrik, Energietraeger, TJ)  # select and reorder columns
 
 print(combined_data)
-```
 
-```{r}
-# create final dataset using existing subsets
 combined_data2 <- bind_rows(combined_data, subset2, subset3, subset4, subset5, subset6, subset7)
 
 combined_gest <- combined_data2 %>%
@@ -116,9 +92,13 @@ combined_gest <- combined_data2 %>%
     Ort = "Kanton ZH" # add canton
   ) %>%
   select(Jahr, Rubrik, Energietraeger, TJ, Einheit, Einheit_lang, Ort)  # select and reorder columns
-```
 
-```{r}
+print(combined_gest)
+
+# # save file as csv - my test
+# write.csv(combined_gest, "combined_gest.csv", row.names = TRUE)
+# head(combined_gest)
+
 # save clean dataset to correct folder, here without row names
 write.csv(combined_gest,"C:/Users/Public/Git Repos/energy/data/output/combined_gest.csv", row.names = FALSE)
-```
+
